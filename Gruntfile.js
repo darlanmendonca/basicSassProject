0cffd380b8def2ac60e8ae0461d400b45265e01c
Andrea Zambrana <azambrana@prestadores.amil.com.br>
2015-12-21 17:38:35 +0000

    Including grunt plugins for css minify and a prod build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,12 @@ module.exports = function(grunt) {
     },
 
     sass: {
+      options: {
+        sourcemap: 'none',
+        lineNumbers: true,
+        style: 'expanded'
+      },
+
       dist: {
         files: {
           '<%= env.buildDir %>/styles/style.css' : '<%= env.appDir %>/sass/style.scss'
@@ -30,11 +36,30 @@ module.exports = function(grunt) {
           {expand: true, cwd: '<%= env.appDir %>/html/', src: ['**'], dest: '<%= env.buildDir %>'}
         ]
       },
-      
+
       images: {
         files: [
-          {expand: true, cwd: '<%= env.appDir %>', src: ['img/**'], dest: '<%= env.buildDir %>'}
+          {expand: true, cwd: '<%= env.appDir %>', src: ['images/**'], dest: '<%= env.buildDir %>'}
         ]
+      },
+
+      resources: {
+        files: [
+          // You can include here pdf, fonts, icons, svg, etc
+          {expand: true, cwd: '<%= env.appDir %>', src: ['resources/**'], dest: '<%= env.buildDir %>'}
+        ]
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= env.buildDir %>/styles',
+          src: ['*.css'],
+          dest: '<%= env.buildDir %>/styles',
+          ext: '.css'
+        }]
       }
     },
 
@@ -47,7 +72,7 @@ module.exports = function(grunt) {
       html: {
         files: ['<%= env.appDir %>/**/*.html'],
         tasks: ['copy:html']
-      },
+      }
     }
   });
 
@@ -55,6 +80,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('build:dev',['clean', 'sass', 'copy:html', 'copy:images', 'watch']);
+  grunt.registerTask('build:dev',['clean', 'sass', 'copy:html', 'copy:images', 'copy:resources', 'watch']);
+  
+  grunt.registerTask('build:prod',['clean', 'sass', 'cssmin', 'copy:html', 'copy:images', 'copy:resources']);
 };
